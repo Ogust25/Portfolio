8b3a7c3213c42f05c839d70cd2f0c0881499a67e
Ogu <august.gros@gmail.com>
2022-04-28 11:33:42 +0200

    add carousel dans boite modal

--- js/script.js
+++ js/script.js
@@ -159,3 +159,16 @@ fetch(url)
     }
 })
 )
+
+
+/* Owl carousel */
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+    });
+});
